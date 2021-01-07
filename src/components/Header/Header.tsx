1d3efce36629b42168acea4ecf4031b6146c14a2
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-08 01:42:17 +0300

    feat: add Header component
    - add react-bootstrap components;

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Nav className="navbar-expand-lg navbar-light bg-light">
+        <Nav.Item>
+          <Nav.Link className="navbar-brand" href="#home" data-testid="header-title">TrackingTime</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </header>
+  );
+}

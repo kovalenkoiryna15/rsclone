998756e2bf94e8c22a1cea64767b5bf08dcf1168
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-01-07 21:22:02 +0300

    fix: TaskListNav comp

--- src/components/TaskListNav/TaskListNav.tsx
+++ src/components/TaskListNav/TaskListNav.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 export default function TaskListNav() {
   return (
     <div className="task-list-nav col-3">
-      <nav className="nav flex-column bg-light">
-        <a className="nav-link active" href="#all-tasks">Tasks</a>
-        <div className="nav-link">Projects</div>
-        <a className="nav-link" href="#no-project">no project</a>
-        <a className="nav-link" href="#project/id">Project A</a>
-      </nav>
+      <Nav className="flex-column bg-light">
+        <Nav.Item>
+          <Nav.Link href="#all-tasks">Tasks</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#projects-tasks">Projects</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#no-project">no project</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#project/id">Project A</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }

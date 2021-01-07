79b25bf68ed533f5adf477f6ed4e1d7c3db6c787
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-08 01:40:17 +0300

    feat: add TaskList component

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+
+export default function TaskList() {
+  return (
+    <Col className="task-list" xs={6}>
+      TaskList
+    </Col>
+  );
+}

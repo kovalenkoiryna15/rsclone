7386ca8b18ddbade03b9132e591bb096a3e47d1d
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-08 01:42:50 +0300

    feat: add index.tsx/scss, App.tsx/scss, serviceWorker, store

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React from 'react';
+import './App.scss';
+
+import Header from './components/Header/Header';
+import MainView from './components/MainView/MainView';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <MainView />
+    </>
+  );
+}

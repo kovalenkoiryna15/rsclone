3acb5c4fbdffe6c82fb92aaf1dc276b0c49072bd
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-08 01:41:59 +0300

    feat: add TaskView component
    - add react-bootstrap components;

---
+++ src/components/TaskView/TaskView.test.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React from 'react';
+import { render } from '@testing-library/react';
+import TaskView from './TaskView';
+
+test('renders TaskView', () => {
+  const { getByText } = render(<TaskView />);
+
+  expect(getByText(/TaskView/i)).toBeInTheDocument();
+});
---
+++ src/components/TaskView/TaskView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+export default function TaskView() {
+  return (
+    <Col className="task-view bg-light" xs={3}>
+      TaskView
+    </Col>
+  );
+}

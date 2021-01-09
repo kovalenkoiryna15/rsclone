cf397bbeea29e90f6187fb19f00df962887b6fad
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-09 18:34:52 +0300

    fix: apply eslint and stylelint
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/MainView/MainView.test.tsx
+++ src/components/MainView/MainView.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import MainView from './MainView';
 
 test('renders MainView', () => {
--- src/components/TaskList/TaskList.test.tsx
+++ src/components/TaskList/TaskList.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import TaskList from './TaskList';
 
 test('renders TaskList', () => {

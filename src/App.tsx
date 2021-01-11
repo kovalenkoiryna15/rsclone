42e03e16e1326705bddb569ee27017e943d80660
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-11 19:05:55 +0300

    fix: replace 'import React' with 'import * as React'
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 import Header from 'Components/Header';
 import MainView from 'Components/MainView';
--- src/components/TaskListNav/TaskListNav.tsx
+++ src/components/TaskListNav/TaskListNav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Col, Nav } from 'react-bootstrap';
 
 export default function TaskListNav() {

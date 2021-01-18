922d5d6616bd928f7c50d4c2ad97cece94415646
Dmitry Bogdan <mgblsttt@gmail.com>
2021-01-18 22:42:03 +0300

    feat: add SideBar component
    Signed-off-by: Dmitry Bogdan <mgblsttt@gmail.com>

--- src/components/TaskListNav/TaskListNav.tsx
+++ src/components/TaskListNav/TaskListNav.tsx
@@ -1,23 +1,26 @@
 import * as React from 'react';
-import { Col, Nav } from 'react-bootstrap';
+import { Col, Nav} from 'react-bootstrap';
+import SideBar from "Components/SideBar";
 
 export default function TaskListNav(): JSX.Element {
   return (
-    <Col className="task-list-nav" xs={3}>
-      <Nav className="flex-column bg-light">
-        <Nav.Item>
-          <Nav.Link href="#all-tasks">Tasks</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#projects-tasks">Projects</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#no-project">no project</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#project/id">Project A</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Col>
+      <SideBar>
+        <Col className="task-list-nav">
+          <Nav className="flex-column bg-light">
+            <Nav.Item>
+              <Nav.Link href="#all-tasks">Tasks</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#projects-tasks">Projects</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#no-project">no project</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#project/id">Project A</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </SideBar>
   );
 }

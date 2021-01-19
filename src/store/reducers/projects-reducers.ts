c140995b789cceabe35a3edbd2cb8385de7876fa
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-01-19 23:34:35 +0300

    feat: reducers: UPDATE_PROJECT, DELETE_PROJECT, FETCH_PROJECTS_SUCCESS, FETCH_PROJECTS_FAILURE, SHOW_LOADER, HIDE_LOADER, S HOW_ERROR

--- src/store/reducers/projects-reducers.ts
+++ src/store/reducers/projects-reducers.ts
@@ -1,13 +1,42 @@
-import { ADD_PROJECT } from '../actions/project-action-constants';
-import { TProjectAction, TInitialProjectsState } from '../actions/project-action-types';
+import {
+  ADD_PROJECT,
+  FETCH_PROJECTS_SUCCESS,
+  SHOW_LOADER,
+  HIDE_LOADER,
+  SHOW_ERROR,
+  UPDATE_PROJECT,
+  DELETE_PROJECT,
+} from '../actions/project-action-constants';
+import {
+  TProjectAction,
+  TInitialProjectsState,
+} from '../actions/project-action-types';
+
+const InitialProjectsState = {
+  projects: [],
+  loading: false,
+  error: null,
+};
 
 export const projectsReducer = (
-  state: TInitialProjectsState = { projects: [] },
-  action: TProjectAction
+  state: TInitialProjectsState = InitialProjectsState,
+  action: TProjectAction,
 ) => {
   switch (action.type) {
     case ADD_PROJECT:
-      return { ...state, projects: [...state.projects, action.newProject] };
+      return { ...state, projects: [...state.projects, action.payload] };
+    case UPDATE_PROJECT:
+      return { ...state, projects: [...state.projects, action.payload] };
+    case DELETE_PROJECT:
+      return { ...state, projects: state.projects.filter((project) => project.id !== action.payload.id) };
+    case FETCH_PROJECTS_SUCCESS:
+      return { ...state, projects: action.payload };
+    case SHOW_LOADER:
+      return { ...state, loading: true };
+    case HIDE_LOADER:
+      return { ...state, loading: false };
+    case SHOW_ERROR:
+      return { ...state, error: action.payload };
     default:
       return state;
   }

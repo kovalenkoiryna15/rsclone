d501224682998464e01afe6297ecb328cfed35aa
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-01-20 00:03:34 +0300

    feat: project-action-types

--- src/store/actions/project-action-types.ts
+++ src/store/actions/project-action-types.ts
@@ -1,18 +1,57 @@
 import { IProject } from '../../entities/project-entities';
 
+export interface TInitialProjectsState {
+  projects: IProject[];
+  loading: boolean;
+  error: Error | null;
+};
+
 export interface TAddProjectAction {
   type: string;
-  newProject: IProject;
-}
+  payload: IProject;
+};
 
-export interface TFetchProjectsAction {
+export interface TUpdateProjectAction {
   type: string;
-  projects: IProject[];
-}
+  payload: IProject;
+};
 
-export interface TInitialProjectsState {
-  projects: IProject[],
-}
+export type TDeleteProjectAction = {
+  type: string;
+  payload: IProject;
+};
 
-export type TProjectAction =
-  | TAddProjectAction;
+export type TFetchProjectsAction = (dispatch: () => void) => Promise<void>;
+
+export interface TShowLoaderAction {
+  type: string;
+  payload?: null;
+};
+
+export interface THideLoaderAction {
+  type: string;
+  payload?: null;
+};
+
+export interface TShowErrorAction {
+  type: string;
+  payload?: null;
+};
+
+export interface TFetchProjectsSuccessAction {
+  type: string;
+  payload: IProject[];
+};
+
+export interface TFetchProjectsFailureAction {
+  type: string;
+  payload: Error | null;
+};
+
+export type TProjectAction = TAddProjectAction
+  | TUpdateProjectAction
+  | TDeleteProjectAction
+  | TFetchProjectsSuccessAction
+  | TFetchProjectsFailureAction
+  | TShowLoaderAction
+  | THideLoaderAction;

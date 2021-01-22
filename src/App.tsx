67183af2c02e957723c28c0e3faa3d86202bc85c
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-22 03:04:57 +0300

    fix: fix styles to avoid scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
+import Container from 'react-bootstrap/Container';
 
 import Header from 'Components/Header';
 import MainView from 'Components/MainView';
 
 export default function App(): JSX.Element {
   return (
-    <>
+    <Container fluid>
       <Header />
       <MainView />
-    </>
+    </Container>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
-import Nav from 'react-bootstrap/Nav';
+import { Navbar, Row } from 'react-bootstrap';
 
 export default function Header(): JSX.Element {
   return (
-    <header className="header">
-      <Nav className="navbar-expand-lg navbar-light bg-light">
-        <Nav.Item>
-          <Nav.Link className="navbar-brand" href="#home" data-testid="header-title">TrackingTime</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </header>
+    <Navbar expand="lg" bg="light">
+      <Navbar.Brand className="navbar-brand" href="#home" data-testid="header-title">TrackingTime</Navbar.Brand>
+    </Navbar>
   );
 }

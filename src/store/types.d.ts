43c0e70a95699d9f660f0e2f41bea644e70a36b7
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-23 04:51:11 +0300

    refactor: apply eslint to 'store/types'
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/store/types.d.ts
+++ src/store/types.d.ts
@@ -9,9 +9,11 @@ export interface IAction<P> {
   payload: P,
 }
 export type Reducer<S, P> = (state: S, action: IAction<P>) => S;
+
 export interface IHandlers<S, P> {
   [key: string]: Reducer<S, P>,
 }
+
 export type AsyncDispatch<T, P> = (
   dispatch: ThunkDispatch<T, any, IAction<P>>,
   getState: () => {

9fa852250886177e8573fce1f3e5148e636a82d2
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-01-24 00:06:26 +0300

    fix: move user store

---
+++ src/store/user/user-reducers.ts
@@ -0,0 +1,52 @@
+import * as MyModels from 'Store/types';
+import { IUser } from 'Entities/user-entities';
+import { IUserState } from 'Store/user/user-types';
+import { ALERT_SUCCESS, ALERT_ERROR } from './alert-action-constants';
+import {
+  REGISTER_SUCCESS,
+  LOGIN_SUCCESS,
+} from './user-action-constants';
+
+const initialState = {
+  user: {
+    id: '',
+    username: '',
+    firstName: '',
+    lastName: '',
+    token: '',
+  },
+  loggingIn: false,
+  alertMessage: null,
+  errorMessage: null,
+};
+
+const handlers: MyModels.IHandlers<IUserState, any> = {
+  [ALERT_SUCCESS]: (state, action: MyModels.IAction<string>) => ({
+    ...state,
+    alertMessage: action.payload,
+  }),
+  [ALERT_ERROR]: (state, action: MyModels.IAction<string>) => ({
+    ...state,
+    alertMessage: action.payload,
+  }),
+  [REGISTER_SUCCESS]: (state, action: MyModels.IAction<IUser>) => ({
+    ...state,
+    user: action.payload,
+    loggingIn: true,
+  }),
+  [LOGIN_SUCCESS]: (state, action: MyModels.IAction<IUser>) => ({
+    ...state,
+    user: action.payload,
+    loggingIn: true,
+  }),
+  DEFAULT: (state) => state,
+};
+
+const userReducer: MyModels.Reducer<IUserState, any> = (
+  state = initialState, action,
+) => {
+  const handle = handlers[action.type] || handlers.DEFAULT;
+  return handle(state, action);
+};
+
+export default userReducer;

20c454d7d030a5f45d2a696dd3caeeaa8ff5d62b
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-23 04:50:51 +0300

    feat:  add fetching tasks once when app start
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 
 import Header from 'Components/Header';
 import MainView from 'Components/MainView';
+import { fetchTasksJSON } from 'Store/task/actions';
 
 export default function App(): JSX.Element {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTasksJSON());
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Container fluid className="app-container">
       <Header />

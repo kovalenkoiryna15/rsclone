0709c68c7da2eb1a1e77c3ebd3f3be427d04872d
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-01-30 03:24:01 +0300

    refactor: add types to firebase.ts
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/store/user/src/firebase.ts
+++ src/store/user/src/firebase.ts
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const REACT_APP_FIREBASE_API_KEY = 'AIzaSyBEWX7BaTSvVglZ0rmnN-5_ai2FN95nL-o';
 const REACT_APP_FIREBASE_AUTH_DOMAIN = 'fake-9d604.firebaseapp.com';
@@ -9,7 +10,7 @@ const REACT_APP_FIREBASE_STORAGE_BUCKET = 'fake-9d604.appspot.com';
 const REACT_APP_FIREBASE_MESSAGING_SENDER_ID = '763790421406';
 const REACT_APP_FIREBASE_APP_ID = '1:763790421406:web:f51492d367ed6677c27a8f';
 
-const app = firebase.initializeApp({
+const app: firebase.app.App = firebase.initializeApp({
   apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: REACT_APP_FIREBASE_DATA_BASE_URL,
@@ -19,5 +20,5 @@ const app = firebase.initializeApp({
   appId: REACT_APP_FIREBASE_APP_ID,
 });
 
-const auth = app.auth();
+const auth: firebase.auth.Auth = app.auth();
 export default auth;

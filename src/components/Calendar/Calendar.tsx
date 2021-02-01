aaf8e5beed755578ce315daab0fe0772a754e4fc
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-02 00:40:31 +0300

    feat: add 'Calendar' component
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,100 @@
+import { useEffect, useRef, useState } from 'react';
+import * as React from 'react';
+import Datetime from 'react-datetime';
+import { Button, Form, Modal } from 'react-bootstrap';
+import moment from 'moment';
+
+interface ICalendarProps {
+  // eslint-disable-next-line react/require-default-props
+  dueDate?: number;
+  setDueDate: (selectedDate?: number) => void;
+  handleShow: () => void;
+  isVisible: boolean;
+}
+
+const Calendar = ({
+  dueDate, setDueDate, handleShow, isVisible,
+}: ICalendarProps): JSX.Element => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  useEffect(() => {
+    setSelectedDate(dueDate ? new Date(dueDate) : new Date());
+  }, [dueDate]);
+
+  const handleSubmit = (
+    event: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLElement>,
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setDueDate(moment(selectedDate).valueOf());
+    handleShow();
+  };
+
+  const handleToday = () => {
+    setDueDate(Date.now());
+    handleShow();
+  };
+
+  const handleClearDate = () => {
+    setDueDate();
+    handleShow();
+  };
+
+  const refDatePicker = useRef(null);
+
+  const buttonClasses = 'text-uppercase font-weight-bold';
+
+  return (
+    <Modal
+      animation
+      className="calendar-modal"
+      onHide={handleShow}
+      show={isVisible}
+    >
+      <Modal.Header>
+        <Button variant="link" onClick={handleToday} className={buttonClasses}>
+          Today
+        </Button>
+        <Button variant="link" onClick={handleClearDate} className={buttonClasses}>
+          Clear Date
+        </Button>
+      </Modal.Header>
+      <Modal.Body className="p-0">
+        <Form
+          className="calendar-form"
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <Form.Group controlId="formDatePicker">
+            <Datetime
+              className="p-0 border-0 bg-dark"
+              initialViewMode="days"
+              input={false}
+              onChange={(newDate) => {
+                setSelectedDate(moment(newDate).toDate());
+              }}
+              open
+              ref={refDatePicker}
+              timeFormat={false}
+              value={selectedDate}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button className={buttonClasses} onClick={handleShow} variant="link">
+          Cancel
+        </Button>
+        <Button
+          className={buttonClasses}
+          onClick={(e) => handleSubmit(e)}
+          type="submit"
+          variant="primary"
+        >
+          Select
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default Calendar;

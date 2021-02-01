736d315e7c06451fc75be726e1a5a0b0d9534019
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-01 04:09:05 +0300

    test: write test for TaskView component
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/TaskView/TaskView.test.tsx
+++ src/components/TaskView/TaskView.test.tsx
@@ -1,10 +1,37 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 
+import ITask from 'Entities/task-entities';
 import TaskView from './TaskView';
 
 test('renders TaskView', () => {
-  const { getByText } = render(<TaskView />);
+  let isVisibleTaskView = true;
+  const setVisibleTaskView = jest
+    .fn()
+    .mockImplementationOnce(() => {
+      isVisibleTaskView = false;
+    })
+    .mockImplementationOnce(() => {
+      isVisibleTaskView = true;
+    })
+    .mockImplementationOnce(() => 'second call')
+    .mockName('setVisibleTaskView');
+  const addTask = jest.fn().mockName('addTask');
+  const updateTask = jest.fn().mockName('updateTask');
+  const newTask: ITask = {
+    id: '',
+    title: '',
+    isCompleted: false,
+  };
+  const { getByText } = render(
+    <TaskView
+      task={newTask}
+      isVisible={isVisibleTaskView}
+      handleShow={setVisibleTaskView}
+      addTask={addTask}
+      updateTask={updateTask}
+    />,
+  );
 
-  expect(getByText(/TaskView/i)).toBeInTheDocument();
+  expect(getByText(/Task Name/i)).toBeInTheDocument();
 });

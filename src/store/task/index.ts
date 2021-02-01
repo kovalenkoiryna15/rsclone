a46c8e25544922e760c84d3b1c92ccd8687fd952
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-01 04:01:29 +0300

    feat: add index.ts to 'store/task/'
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

---
+++ src/store/task/index.ts
@@ -0,0 +1,11 @@
+import * as tasksActions from './actions';
+import * as tasksConstants from './action-constants';
+import * as tasksTypes from './action-types';
+import tasksReducer from './reducers';
+
+export {
+  tasksActions,
+  tasksConstants,
+  tasksReducer,
+  tasksTypes,
+};

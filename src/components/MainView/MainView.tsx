fcc75be1e07377521112d232cc1c52995d061f16
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-02-02 00:16:51 +0300

    feat: tasklists pages routing

--- src/components/MainView/MainView.tsx
+++ src/components/MainView/MainView.tsx
@@ -1,16 +1,45 @@
 import * as React from 'react';
-import { Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import * as MyModels from 'Store/types';
 import TaskListContainer from 'Components/TaskList/TaskListContainer';
 import TaskListNav from 'Components/TaskListNav';
-import TaskView from 'Components/TaskView';
+import IProject from 'Entities/project-entities';
 
 export default function MainView(): JSX.Element {
+  const allProjects = useSelector((state: MyModels.RootState) => {
+    const { projects: { projects } } = state;
+    return projects;
+  });
+
+  function renderTaskListPages() {
+    return allProjects ? (
+      Object.values(allProjects).map((project: IProject) => {
+        const { id } = project;
+        return (
+          <Route path={`/projects/${id}`} key={id}>
+            <TaskListContainer id={id} />
+          </Route>
+        );
+      })
+    ) : null;
+  }
+
   return (
     <Row className="main-view">
-      <TaskListNav />
-      <TaskListContainer />
-      <TaskView />
+      <BrowserRouter>
+        <TaskListNav />
+        <Col className="task-list">
+          <Switch>
+            <Route exact path="/">
+              <TaskListContainer id="" />
+            </Route>
+            {renderTaskListPages()}
+          </Switch>
+        </Col>
+      </BrowserRouter>
     </Row>
   );
 }

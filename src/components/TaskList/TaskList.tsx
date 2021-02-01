c404526c8a40678a3e3af680887efb7f3a8657ea
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-01 17:14:51 +0300

    feat: add 'TaskView' and button '+ Task' to 'TaskList'
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { Button, Col, Nav } from 'react-bootstrap';
 
-import ITask from 'Entities/task-entities';
 import TaskItem from 'Components/TaskItem';
+import TaskView from 'Components/TaskView';
+import ITask from 'Entities/task-entities';
 
 interface ITaskListProps {
   tasks: Array<ITask>;
@@ -9,32 +12,59 @@ interface ITaskListProps {
   id: string;
 }
 
-const TaskList = ({ tasks, isLoading, id }: ITaskListProps): JSX.Element => (
-  <>
-    {
-      tasks.length
-        ? (
-          <ul className="list-group">
-            {
-              id
-                ? tasks.filter((task) => task.project === id).map((task) => (
-                  <TaskItemContainer
-                    task={task}
-                    key={task.id.toString()}
-                  />
-                ))
-                : tasks.map((task) => (
-                  <TaskItem
-                    task={task}
-                    key={task.id.toString()}
-                  />
-                ))
-            }
-          </ul>
-        )
-        : !isLoading && <p>No tasks!</p>
-    }
-  </>
-);
+const defaultTask: ITask = {
+  id: '',
+  title: '',
+  isCompleted: false,
+};
+
+const TaskList = ({ tasks, isLoading, id }: ITaskListProps): JSX.Element => {
+  const [isVisibleTaskView, setVisibleTaskView] = useState(false);
+  const [openedTask, setOpenedTask] = useState(defaultTask);
+
+  const handleShow = () => {
+    setVisibleTaskView(!isVisibleTaskView);
+  };
+
+  return (
+    <Col className="task-list">
+      <Nav className="justify-content-end" activeKey="/home">
+        <Nav.Item>
+          <Button onClick={handleShow} className="text-uppercase text-nowrap">
+            + Task
+          </Button>
+        </Nav.Item>
+      </Nav>
+      {
+        tasks.length
+          ? (
+            <ul className="list-group">
+              {
+                id
+                  ? tasks.filter((task) => task.project === id).map((task) => (
+                    <TaskItem
+                      task={task}
+                      key={task.id.toString()}
+                    />
+                  ))
+                  : tasks.map((task) => (
+                    <TaskItem
+                      task={task}
+                      key={task.id.toString()}
+                    />
+                  ))
+              }
+            </ul>
+          )
+          : !isLoading && <p>No tasks!</p>
+      }
+      <TaskView
+        task={openedTask}
+        isVisible={isVisibleTaskView}
+        handleShow={handleShow}
+      />
+    </Col>
+  );
+};
 
 export default TaskList;

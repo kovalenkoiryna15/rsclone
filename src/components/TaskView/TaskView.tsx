daef4d5292e6ee28bc2869e93716897f0f3ad289
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-01 04:07:42 +0300

    feat: implement basic view of TaskView component
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/TaskView/TaskView.tsx
+++ src/components/TaskView/TaskView.tsx
@@ -1,10 +1,120 @@
+import { useState } from 'react';
 import * as React from 'react';
-import { Col } from 'react-bootstrap';
+import { Modal, Button, Form } from 'react-bootstrap';
+import EventIcon from '@material-ui/icons/Event';
 
-export default function TaskView(): JSX.Element {
-  return (
-    <Col className="task-view" sm={12} md={3} lg={3}>
-      TaskView
-    </Col>
-  );
+import ITask from 'Entities/task-entities';
+import { tasksTypes } from 'Store/task';
+import * as MyModels from 'Store/types';
+
+interface ITaskViewProps {
+  task?: ITask;
+  addTask: (task: Omit<ITask, 'id'>) => MyModels.AsyncDispatch<tasksTypes.TasksState, any>;
+  updateTask: (task: ITask) => MyModels.AsyncDispatch<tasksTypes.TasksState, any>;
+  handleShow: () => void;
+  isVisible: boolean;
 }
+
+interface ITaskViewState {
+  task: ITask;
+  isValidated: boolean;
+}
+
+const initialState: Readonly<ITaskViewState> = {
+  task: {} as ITask,
+  isValidated: false,
+};
+
+const TaskView = ({
+  task = {} as ITask, addTask, updateTask, isVisible, handleShow,
+}: ITaskViewProps): JSX.Element => {
+  const [state, setState] = useState<ITaskViewState>({ ...initialState, task });
+  const { isValidated } = state;
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const { currentTarget: form } = event;
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setState((prevState) => ({
+      ...prevState,
+      isValidated: true,
+    }));
+
+    const { task: newTask } = state;
+    const { id } = newTask;
+
+    if (id) {
+      updateTask({ ...newTask, id });
+    } else {
+      addTask(newTask);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    setState((prevState) => {
+      const { name, value } = event.target;
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const { title } = task;
+
+  return (
+    <Modal
+      animation
+      className="task-modal"
+      onHide={handleShow}
+      show={isVisible}
+    >
+      <Modal.Header closeButton>
+        <Button variant="primary">
+          <EventIcon />
+          Due date
+        </Button>
+      </Modal.Header>
+      <Modal.Body>
+        <Form
+          className="task-form"
+          validated={isValidated}
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}
+        >
+          <Form.Group controlId="formTaskName">
+            <Form.Label>Task Name</Form.Label>
+            <Form.Control
+              name="title"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)}
+              placeholder="Task Name"
+              required
+              type="text"
+              value={title}
+            />
+          </Form.Group>
+          <Button
+            variant="secondary"
+            onClick={handleShow}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={(e: React.MouseEvent<HTMLFormElement>) => handleSubmit(e)}
+          >
+            Save
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default TaskView;

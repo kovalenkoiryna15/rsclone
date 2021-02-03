fa99324f5e7407dba791f3020ae80ed6622baa9b
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-03 03:47:41 +0300

    refactor: remove actions and constants function from 'store/task/'
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/store/task/actions.ts
+++ src/store/task/actions.ts
@@ -1,11 +1,7 @@
-import IRSCloneTrackingTime from 'Entities/rsclone-tracking-time';
 import ITask from 'Entities/task-entities';
 import * as Types from 'Entities/types';
-import * as React from 'react';
-import { IProjectState } from 'Store/project/action-types';
 import { TasksState } from 'Store/task/action-types';
 import * as MyModels from 'Store/types';
-import { IAction } from 'Store/types';
 import {
   deleteTask,
   getTasks,
@@ -20,17 +16,9 @@ import {
   FETCH_TASKS,
   FETCH_TASKS_FAILURE,
   REMOVE_TASK,
-  SET_NEW_TASK_TITLE,
   UPDATE,
   UPDATE_FAILURE,
 } from './action-constants';
-
-const JSON_URL = 'https://kovalenkoiryna15.github.io/fake-projects/db.json';
-
-export const setTitle = (title: string): IAction<string> => ({
-  type: SET_NEW_TASK_TITLE,
-  payload: title,
-});
 
 export const update = (
   task: ITask,
@@ -103,31 +91,5 @@ export const removeTask = (id: Types.ID): MyModels.AsyncDispatch<TasksState, any
     type: REMOVE_TASK,
     payload: id,
   });
-  dispatch(hideLoader());
-};
-
-export const onChange = (e: React.ChangeEvent<HTMLInputElement>): IAction<string> => ({
-  type: SET_NEW_TASK_TITLE,
-  payload: e.target.value,
-});
-
-export const fetchTasksJSON = (): MyModels.AsyncDispatch<IProjectState, any> => async (
-  dispatch,
-) => {
-  dispatch(showLoader());
-  try {
-    const response: Response = await fetch(JSON_URL);
-    const { tasks } = await response.json() as IRSCloneTrackingTime;
-    dispatch({
-      type: FETCH_TASKS,
-      payload: tasks,
-    });
-  } catch (error) {
-    dispatch({
-      type: FETCH_TASKS_FAILURE,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      payload: error,
-    });
-  }
   dispatch(hideLoader());
 };

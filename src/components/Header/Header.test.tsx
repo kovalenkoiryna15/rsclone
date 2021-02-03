880671f2e2fd78e021cbff40ae98668556e3bbda
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-03 07:24:17 +0300

    fix: tests in Header.test.tsx and TaskListNav.test.tsx
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,16 @@
-import * as React from 'react';
 import { render } from '@testing-library/react';
+import * as React from 'react';
+import { Provider } from 'react-redux';
 
+import store from 'Store/store';
 import Header from './Header';
 
 test('renders Header', () => {
-  const { getByText } = render(<Header />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <Header />
+    </Provider>,
+  );
 
   expect(getByText(/RSClone Tracking Time/i)).toBeInTheDocument();
 });

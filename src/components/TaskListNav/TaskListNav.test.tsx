cd48b97925368d91cb84f5b08a085aa87e9739e9
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-03 07:24:17 +0300

    fix: tests in Header.test.tsx and TaskListNav.test.tsx
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/TaskListNav/TaskListNav.test.tsx
+++ src/components/TaskListNav/TaskListNav.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import * as React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import store from 'Store/store';
 import TaskListNav from './TaskListNav';
@@ -9,7 +10,9 @@ test('renders TaskListNav', () => {
   const fakeProjects = { key: { id: 'id', title: 'title' } };
   const { getByText } = render(
     <Provider store={store}>
-      <TaskListNav projects={fakeProjects} isLoading={false} />
+      <Router>
+        <TaskListNav projects={fakeProjects} isLoading={false} />
+      </Router>
     </Provider>,
   );
 

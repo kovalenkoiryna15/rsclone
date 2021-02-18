4172d4b990d3758b635ac2bd615262dc06b0c735
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-19 02:54:00 +0300

    refactor: rename redux-states/store.ts to root-store.ts
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 
-import store from 'States/store';
+import store from 'States/root-store';
 import App from './App';
 
 test('renders App', () => {

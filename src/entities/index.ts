9631b63f1696ebd2b03b46a597ecdffc067b851b
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-19 02:31:09 +0300

    feat: add index.ts in src/entities
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

---
+++ src/entities/index.ts
@@ -0,0 +1,9 @@
+import IProject from 'Entities/project-entity';
+import IProjects from 'Entities/projects-entity';
+import IRSCloneTrackingTime from 'Entities/rsclone-tracking-time';
+import ITask from 'Entities/task-entities';
+import ITimeEntry from 'Entities/time-entry';
+import { ID } from 'Entities/types';
+import IUser from 'Entities/user-entities';
+
+export type { IProject, IProjects, IRSCloneTrackingTime, ITask, ITimeEntry, IUser, ID };

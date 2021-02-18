a3ebc2e2e05babe0cde94af2879bcfae1fc500ca
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-18 23:07:46 +0300

    test(jest): move __mocks__ directory from root to /src
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
+      '<rootDir>/src/__mocks__/fileMock.js',
     '\\.(css|scss)$': 'identity-obj-proxy',
 
     '^Assets/(.*)$': '<rootDir>/src/assets/$1',
@@ -19,7 +19,7 @@ module.exports = {
     '^Store/(.*)$': '<rootDir>/src/store/$1',
     '^Styles/(.*)$': '<rootDir>/src/styles/$1',
   },
-  roots: ['<rootDir>/src/'],
+  roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testMatch: ['./**/*.{spec,test}.{ts,tsx,js,jsx}'],
   verbose: true,

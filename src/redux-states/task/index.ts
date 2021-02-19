7a513989285a930b1273246bbf3e932385a33117
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-19 03:15:16 +0300

    refactor: rename src/task/action-types.ts to types.ts
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/redux-states/task/index.ts
+++ src/redux-states/task/index.ts
@@ -1,6 +1,6 @@
 import * as taskActions from './actions';
 import * as taskConstants from './action-constants';
-import * as taskTypes from './action-types';
+import * as taskTypes from './types';
 import taskReducer from './reducer';
 
 export { taskActions, taskConstants, taskReducer, taskTypes };

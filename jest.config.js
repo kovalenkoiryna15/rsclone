91485f51cde36f0cd1d3ddb89eea83b3bee9be14
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-24 17:31:23 +0300

    feat: exclude *.d.ts files in jest.config.js
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   collectCoverage: true,
-  collectCoverageFrom: ['*.{ts,tsx}'],
+  collectCoverageFrom: ['*.{ts,tsx}', '!*.d.ts'],
   coverageDirectory: './coverage/',
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
